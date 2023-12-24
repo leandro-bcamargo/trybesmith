0e00e08657c2cc4d7e0ec2ae4bfc2ad1c97e2fc1
Leandro Camargo <leandro.bcamargo@gmail.com>
2023-12-24 19:19:35 -0300

    remoção de logs e comentários

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -5,20 +5,15 @@ import CustomError from "../utils/CustomError";
 import { userModel } from "../models";
 import { RequestWithUser } from "../types/Request";
 
-// function extractToken(authorization: string) {
-//   return authorization.split(' ')[1];
-// }
-
 async function authMiddleware(req: RequestWithUser, res: Response, next: NextFunction) {
   const { authorization: token } = req.headers;
 
   if (!token) {
     throw new CustomError('UNAUTHORIZED', 'Token not found')
   }
-  // const token = extractToken(authorization);
+
   const { username }: TokenPayload = jwtUtils.verify(token);
   const user = await userModel.getByUsername(username);
-  console.log('authMiddleware user:', user)
 
   if (!user) {
     throw new CustomError('UNAUTHORIZED', 'Invalid token')
